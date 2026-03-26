d86fc11109b78af6fa42d15af29d4cbbcb18ed2c
Youssef_ALI <157065490+sofa314@users.noreply.github.com>
2026-03-26 12:57:04 +0200

    Configure base path and API_KEY in vite config
    Set base path for deployment and ensure API_KEY is defined.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/portoflie/', // 👈 IMPORTANT (repo name)
+
   define: {
-    // This allows process.env.API_KEY to be available during the build
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
   },
+
   build: {
     outDir: 'dist',
     rollupOptions: {
